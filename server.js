7764a471f46482c07259c1547940f64bbaf5642c
Mohking1 <lokhandwalamohammed100@gmail.com>
2024-06-07 17:44:26 +0530

    Can accept URL

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const { createClient } = require('@supabase/supabase-js');
 const { v4: uuidv4 } = require('uuid');
 const { subscribeToProgressUpdates } = require('./progress');
@@ -8,6 +7,8 @@ const { sendImage } = require('./sendImages');
 const multer = require('multer');
 const path = require('path');
 const { extractPNGMetadata } = require('./png-metadata');
+const fs = require('fs');
+const axios = require('axios');
 
 const app = express();
 
@@ -17,7 +18,6 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -30,19 +30,44 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
+const downloadImage = async (imageUrl, requestId) => {
+  const imagePath = path.join('uploads', `${requestId}.png`);
+  const writer = fs.createWriteStream(imagePath);
+
+  const response = await axios({
+    url: imageUrl,
+    method: 'GET',
+    responseType: 'stream'
+  });
+
+  response.data.pipe(writer);
+
+  return new Promise((resolve, reject) => {
+    writer.on('finish', () => resolve(imagePath));
+    writer.on('error', err => reject(err));
+  });
+};
 
 app.post('/upload', upload.single('image'), async (req, res) => {
-  const { model_name, scale } = req.body;
+  const { model_name, scale, imageUrl } = req.body;
   const height = req.body.height || null;
   const width = req.body.width || null;
-  const imagePath = req.file.path;
+  let imagePath = req.file ? req.file.path : null;
 
-  if (!imagePath || !model_name || !scale) {
-    return res.status(400).send({ error: 'Image, model_name, and scale are required' });
+  if (!imagePath && !imageUrl) {
+    return res.status(400).send({ error: 'Either an image file or an image URL is required' });
+  }
+
+  if (!model_name || !scale) {
+    return res.status(400).send({ error: 'model_name and scale are required' });
   }
 
   try {
-    const requestId = path.basename(req.file.path, path.extname(req.file.path));
+    const requestId = imageUrl ? uuidv4() : path.basename(req.file.path, path.extname(req.file.path));
+
+    if (imageUrl) {
+      imagePath = await downloadImage(imageUrl, requestId);
+    }
 
     const { data, error } = await supabase
       .from('requests')
@@ -62,7 +87,6 @@ app.post('/upload', upload.single('image'), async (req, res) => {
     subscribeToProgressUpdates(requestId, model_name, height, width, scale);
     extractPNGMetadata(imagePath, requestId);
     res.status(200).send({ requestId });
-    
   } catch (error) {
     console.error('Error uploading image:', error);
     res.status(500).send({ error: 'Internal Server Error' });
